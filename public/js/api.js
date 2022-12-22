c9d4de9254d8108d1f4d9f674095e1e46ba4bc35
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-22 11:57:07 -0300

    feat: featch

---
+++ public/js/api.js
@@ -0,0 +1,4 @@
+const API_KEY = '7ac2dfa5907cc5834071c91077526e17';
+const img_url = 'https://image.tmdb.org/t/p/w500';
+const genres_list_http = 'https://api.themoviedb.org/3/genre/movie/list?';
+const movie_list_http = 'https://api.themoviedb.org/3/discover/movie?';
